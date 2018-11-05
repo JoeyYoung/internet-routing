d0f4a9556114d936e4df91b9a2e72006c8ed8b35
JoeyYoung <1761889730@qq.com>
2018-11-06 01:40:32 +0800

    build basic framework

---
+++ Simulation-verify actions/actor.py
@@ -0,0 +1,62 @@
+import numpy as np
+import tensorflow as tf
+
+'''
+    for each agent, state features are different, create for each
+    each time epoch, use one s,a,r to single train
+'''
+
+
+class Actor:
+    def __init__(self, sess, n_features, n_actions, id, lr=0.001):
+        self.id = id
+        self.sess = sess
+        # state: observable end-to-end throughput + neighbor end-to-end throughput
+        self.s = tf.placeholder(tf.float32, [1, n_features], name='state')
+        self.a = tf.placeholder(tf.int32, None, name='action')
+        self.td_error = tf.placeholder(tf.float32, None, name='td-error')
+
+        with tf.variable_scope('Actor' + str(id)):
+            l1 = tf.layers.dense(
+                inputs=self.s,
+                units=20,
+                activation=tf.nn.relu,
+                kernel_initializer=tf.random_normal_initializer(mean=0, stddev=0.1),
+                bias_initializer=tf.constant_initializer(0.1),
+                name='l1'
+            )
+
+            self.acts_prob = tf.layers.dense(
+                inputs=l1,
+                units=n_actions,
+                activation=tf.nn.softmax,
+                kernel_initializer=tf.random_normal_initializer(mean=0, stddev=0.1),
+                bias_initializer=tf.constant_initializer(0.1),
+                name='acts_prob'
+            )
+
+            with tf.variable_scope('exp_v'):
+                # how to consider self.a
+                log_prob = tf.log(self.acts_prob[0, self.a])
+                self.exp_v = tf.reduce_mean(log_prob * self.td_error)
+
+            with tf.variable_scope('train'):
+                self.train_op = tf.train.AdamOptimizer(lr).minimize(-self.exp_v)
+
+    def learn(self, s, a, td):
+        # add a dim(sample num), here is single sample train
+        s = s[np.newaxis, :]
+        feed_dict = {self.s: s, self.a: a, self.td_error: td}
+        _, exp_v = self.sess.run([self.train_op, self.exp_v], feed_dict=feed_dict)
+        return exp_v
+
+    def choose_action(self, s):
+        s = s[np.newaxis, :]
+        probs = self.sess.run(self.acts_prob, feed_dict={self.s: s})
+        # note the bug: for -1
+        return np.random.choice(np.arange(probs.shape[1]), p=probs.ravel())
+
+# sess = tf.Session()
+# sess.run(tf.global_variables_initializer())
+# actor = Actor(sess, 10, 20)
+# np.array([1.23, 2.2, 3.3, 4.43, 5.4, 6.3, 7.3, 8.3, 9.3, 10.2]), [1, 2], 0.23
---
+++ Simulation-verify actions/agent.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+class Agent:
+    def __init__(self, id, paths):
+        self.id = id
+        self.paths = paths
+
+        # A-C basic framework
+        self.actor = None
+        self.critic = None
+
+        self.action_labels = self.init_action_label()
+        self.n_actions = len(self.action_labels)
+
+    #  TODO redefine labels, index is label, element is action vector for differ-dst flows
+    def init_action_label(self):
+        action_labels = []
+        if self.id is 0:
+            for i in self.paths[1]:
+                for j in self.paths[2]:
+                    for k in self.paths[3]:
+                        for m in self.paths[4]:
+                            acts = []
+                            acts.append(-1)  # represent this
+                            acts.append(i)
+                            acts.append(j)
+                            acts.append(k)
+                            acts.append(m)
+                            action_labels.append(acts)
+
+        if self.id is 1:
+            for i in self.paths[0]:
+                for j in self.paths[2]:
+                    for k in self.paths[3]:
+                        for m in self.paths[4]:
+                            acts = []
+                            acts.append(i)
+                            acts.append(-1)
+                            acts.append(j)
+                            acts.append(k)
+                            acts.append(m)
+                            action_labels.append(acts)
+
+        if self.id is 2:
+            for i in self.paths[0]:
+                for j in self.paths[1]:
+                    for k in self.paths[3]:
+                        for m in self.paths[4]:
+                            acts = []
+                            acts.append(i)
+                            acts.append(j)
+                            acts.append(-1)  # represent this
+                            acts.append(k)
+                            acts.append(m)
+                            action_labels.append(acts)
+
+        if self.id is 3:
+            for i in self.paths[0]:
+                for j in self.paths[1]:
+                    for k in self.paths[2]:
+                        for m in self.paths[4]:
+                            acts = []
+                            acts.append(i)
+                            acts.append(j)
+                            acts.append(k)
+                            acts.append(-1)  # represent this
+                            acts.append(m)
+                            action_labels.append(acts)
+
+        if self.id is 4:
+            for i in self.paths[0]:
+                for j in self.paths[1]:
+                    for k in self.paths[2]:
+                        for m in self.paths[3]:
+                            acts = []
+                            acts.append(i)
+                            acts.append(j)
+                            acts.append(k)
+                            acts.append(m)
+                            acts.append(-1)  # represent this
+                            action_labels.append(acts)
+        return action_labels
+
+    def set_rl_setting(self, actor, critic):
+        self.actor = actor
+        self.critic = critic
